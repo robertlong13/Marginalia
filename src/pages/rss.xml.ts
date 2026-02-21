14e16435f97a4c5b97fab6ded9460711ffd55d21
Bob Long <robertlong13@gmail.com>
2026-02-21 12:16:01 +1100

    Initial commit

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,19 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import type { APIContext } from 'astro';
+
+export async function GET(context: APIContext) {
+  const posts = await getCollection('posts');
+  return rss({
+    title: 'Marginalia',
+    description: 'Scattered commentary over a wide range of things.',
+    site: context.site!,
+    items: posts
+      .sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf())
+      .map((post) => ({
+        title: post.data.title,
+        pubDate: post.data.date,
+        link: `/posts/${post.id}/`,
+      })),
+  });
+}
