6c1bc0e21cf6feb20edb068293169bec298938f5
Bob Long <robertlong13@gmail.com>
2026-02-27 01:14:50 +1100

    Set site URL and base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com', // Replace with your custom domain or GitHub Pages URL
+  site: 'https://robertlong13.github.io',
+  base: '/Marginalia',
   output: 'static',
 });
